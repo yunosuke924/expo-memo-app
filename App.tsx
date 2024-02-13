dc7c432b841c04d50141b2c74ad7d7df701ca4f1
yunosuke924 <y.minegishi0924@gmail.com>
2024-02-14 02:43:10 +0900

    add: Helloと表示

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import { Hello } from './src/components/Hello'
 
-export default function App() {
+export default function App():JSX.Element {
   return (
     <View style={styles.container}>
       <Text>簡単なメモアプリ</Text>
+      <Hello />
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
---
+++ src/components/Hello.tsx
@@ -0,0 +1,3 @@
+import { View, Text } from 'react-native'
+
+export const Hello = (): JSX.Element => <View><Text>hello</Text></View>
