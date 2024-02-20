6aca32ee0447a8e9801bb109b8ab9026c4042d77
yunosuke924 <y.minegishi0924@gmail.com>
2024-02-20 23:01:45 +0900

    add: 買い物リストの構造体の下地作成

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,13 +2,54 @@ import { View, Text, StyleSheet } from 'react-native'
 
 const Index = (): JSX.Element => {
   return (
-    <View>
-      <Text style={Styles.text}>hello</Text>
+    <View style={Styles.constainer}>
+      {/* ヘッダー */}
+      <View>
+        <View>
+          <Text style={Styles.text}>hello</Text>
+        </View>
+        <View>
+          <Text>ログアウト</Text>
+        </View>
+      </View>
+
+      {/* 買い物リスト */}
+      <View>
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2024年2月20日</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2024年2月20日</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+      </View>
+
+      {/* 追加ボタン */}
+      <View>
+        <Text>+</Text>
+      </View>
     </View>
   )
 }
 
 const Styles = StyleSheet.create({
+  constainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
   text: {
     backgroundColor: 'blue',
     color: 'white',
