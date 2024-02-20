d91d08fcd7114144c7e2b45f7e83425143dac54f
yunosuke924 <y.minegishi0924@gmail.com>
2024-02-20 23:43:31 +0900

    add: メモ一覧のスタイル追加

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,31 +4,46 @@ const Index = (): JSX.Element => {
   return (
     <View style={Styles.constainer}>
       {/* ヘッダー */}
-      <View>
-        <View>
-          <Text style={Styles.text}>hello</Text>
-        </View>
-        <View>
-          <Text>ログアウト</Text>
+      <View style={Styles.header}>
+        <View style={Styles.headerInner}>
+          <Text style={Styles.headerTitle}>メモ管理アプリ</Text>
+          <Text style={Styles.headerRight}>ログアウト</Text>
         </View>
       </View>
 
       {/* 買い物リスト */}
       <View>
-        <View>
+        <View style={Styles.memoListItem}>
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2024年2月20日</Text>
+            <Text style={Styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={Styles.memoListItemDate}>2024年2月20日</Text>
           </View>
           <View>
             <Text>X</Text>
           </View>
         </View>
-
-        <View>
+        <View style={Styles.memoListItem}>
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2024年2月20日</Text>
+            <Text style={Styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={Styles.memoListItemDate}>2024年2月20日</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+        <View style={Styles.memoListItem}>
+          <View>
+            <Text style={Styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={Styles.memoListItemDate}>2024年2月20日</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+        <View style={Styles.memoListItem}>
+          <View>
+            <Text style={Styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={Styles.memoListItemDate}>2024年2月20日</Text>
           </View>
           <View>
             <Text>X</Text>
@@ -37,8 +52,8 @@ const Index = (): JSX.Element => {
       </View>
 
       {/* 追加ボタン */}
-      <View>
-        <Text>+</Text>
+      <View style={Styles.circleButton}>
+        <Text style={Styles.circleButtonLabel}>+</Text>
       </View>
     </View>
   )
@@ -46,15 +61,71 @@ const Index = (): JSX.Element => {
 
 const Styles = StyleSheet.create({
   constainer: {
-    flex: 1,
-    justifyContent: 'center',
+    flex: 1, // 画面いっぱいに広げる
+    backgroundColor: '#fffff'
+  },
+  header: {
+    backgroundColor: '#467FD3',
+    height: 104,
+    justifyContent: 'flex-end'
+  },
+  headerInner: {
     alignItems: 'center'
   },
-  text: {
-    backgroundColor: 'blue',
-    color: 'white',
+  headerRight: {
+    position: 'absolute',
+    right: 16,
+    bottom: 16,
+    color: 'rgba(255,255,255,0.7)'
+  },
+  headerTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    fontWeight: 'bold',
+    color: '#ffffff'
+  },
+  memoListItem: {
+    backgroundColor: '#ffffff',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 16,
+    paddingHorizontal: 19,
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: 'rgba(0,0,0,0.15)'
+  },
+  memoListItemTitle: {
+    fontSize: 16,
+    lineHeight: 32
+  },
+  memoListItemDate: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: '#848484'
+  },
+  circleButton: {
+    width: 64,
+    height: 64,
+    backgroundColor: '#467FD3',
+    borderRadius: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    right: 40,
+    bottom: 40,
+    // 以下は影の設定だけどiOSのみ
+    shadowColor: '#000000',
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    shadowOffset: { width: 0, height: 2 },
+    // 以下はAndroidのみ
+    elevation: 8 // elevationはGoogle Material Designの層の高さの設定
+  },
+  circleButtonLabel: {
+    color: '#ffffff',
     fontSize: 40,
-    padding: 8
+    lineHeight: 40
   }
 })
 
