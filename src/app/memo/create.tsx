f6e585c3ec65a4cb57283605b6c3000f66d06392
yunosuke924 <y.minegishi0924@gmail.com>
2024-02-23 18:22:57 +0900

    add: 作成ページ

---
+++ src/app/memo/create.tsx
@@ -0,0 +1,37 @@
+import { View, StyleSheet, TextInput, KeyboardAvoidingView } from 'react-native'
+import Header from '@/components/Header'
+import CircleButton from '@/components/CircleButton'
+import Icon from '@/components/Icon'
+
+const Create = (): JSX.Element => {
+  return (
+    <KeyboardAvoidingView style={styles.container} behavior='height'>
+      <Header />
+      <View style={styles.inputContainer}>
+        <TextInput value='' style={styles.input} multiline />
+      </View>
+        <CircleButton>
+          <Icon name="check" size={40} color='#fff'/>
+        </CircleButton>
+    </KeyboardAvoidingView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  inputContainer: {
+    paddingVertical: 32,
+    paddingHorizontal: 27,
+    flex: 1
+  },
+  input: {
+    flex: 1,
+    textAlignVertical: 'top',
+    fontSize: 16,
+    lineHeight: 24
+  }
+})
+
+export default Create
