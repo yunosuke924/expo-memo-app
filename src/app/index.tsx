bf1641bfa14b9fae5bbdc11b48be4b2f2ce1c3c9
yunosuke924 <y.minegishi0924@gmail.com>
2024-02-23 18:24:07 +0900

    fix: ログインページの検証

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@ import { Redirect } from 'expo-router'
 
 const Index = (): JSX.Element => {
   // メモ一覧画面にリダイレクト
-  return <Redirect href='memo/detail' />
+  return <Redirect href='auth/log_in' />
 }
 
 export default Index
